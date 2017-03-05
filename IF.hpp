300a56206e1cdad1045794069a04e1a74212686a
Anlo7085 <anlo7085@colorado.edu>
2017-03-04 20:58:22 -0700

    Update IF.hpp

--- IF.hpp
+++ IF.hpp
@@ -10,6 +10,16 @@
 #define IF_hpp
 
 #include <stdio.h>
+
+
+//Type Determination 
+#define     rtype           0x00000000
+#define     itype           0x00000001
+#define     jtype           0x00000010
+#define     trap_i_type     0x00000011
+#define     br_coprocessor  0x00000100
+
+
 //Masks for Determining Values
 #define     opcode_mask     0xFC000000
 #define     function_mask   0x0000003F
@@ -19,10 +29,11 @@
 #define     shamt_mask      0x000007C0
 #define     immed16_mask    0x0000FFFF
 #define     immed26_mask    0x03FFFFFF
+#define     br_coprocessor  0x00030000
+
 
 //Opcodes for every MIPS function
 #define     lw_opcode       0b100011
-#define     r-format        0b000000
 #define     addi_opcode     0b001000
 #define     sw_opcode       0b101011
 #define     slti_opcode     0b001010
@@ -33,6 +44,7 @@
 #define     ori_opcode      0b001101
 #define     xori_opcode     0b001110
 #define     lb_opcode       0b100000
+#define     lbu_opcode      0b100100
 #define     lh_opcode       0b100001
 #define     sb_opcode       0b101000
 #define     sh_opcode       0b101001
@@ -41,6 +53,23 @@
 #define     j_opcode        0b000010
 #define     jal_opcode      0b000011
 #define     lhu_opcode      0b100101
+#define     lwcl_opcode     0b110001
+#define     lwl_opcode      0b100010
+#define     lwr_opcode      0b100110
+#define     ll_opcode       0b110000
+#define     swcl_opcode     0b110001
+#define     sdcl_opcode     0b111101
+#define     swl_opcode      0b101010
+#define     swr_opcode      0b101110
+#define     sc_opcode       0b111000
+//#define     bclf_opcode     0b010001
+//#define     bclt_opcode     0b
+//#define     bgez_opcode
+#define     bgtz_opcode     0b000111
+#define     blez_opcode     0b000110
+//#define     bltzal          0b
+//#define     bltz            0b
+#define     
 
 
 //Fucntions for R-format MIPS
@@ -75,7 +104,7 @@
 #define     tltu_func       0b110011
 
 //Special Functions 
-#define     special_opcode  0b011100
+#define     special_opcode1 0b011100      //0x1C
 #define     clo_func        0b100001
 #define     clz_func        0b100000
 #define     mul_func        0b000010
@@ -83,5 +112,10 @@
 #define     maddu_func      0b000001
 #define     msub_func       0b000100
 #define     msubu_func      0b000101
+
+#define     special_opcode2 0b010001
+#define     bclt_func       0b000001
+#define     bclf_func       0b000000
+
 
 #endif /* IF_hpp */
