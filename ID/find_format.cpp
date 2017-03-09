36c26cecc59449c5501db5d8618820f1f06c9bff
Anlo7085 <anlo7085@colorado.edu>
2017-03-08 20:38:22 -0700

    Update find_format.cpp

--- ID/find_format.cpp
+++ ID/find_format.cpp
@@ -2,32 +2,32 @@
 #include <iostream>
 std::vector<int> find_format(unsigned int instr)
 {
-    int opcode = find_opcode(instr);
-    if(opcode == 0)
+    Shadow_IDEX.opcode = find_opcode(instr);
+    if(Shadow_IDEX.opcode == 0)
     {
-        int func = find_func(instr)
-        return std::vector<int> format ({rtype, opcode, func}); 
+        Shadow_IDEX.func = find_func(instr)
+        return std::vector<int> format ({rtype, Shadow_IDEX.opcode, Shadow_IDEX.func}); 
     }
-    if(((opcode >= 0x5) && (opcode <= 0xF)) || (opcode == 4) || ((opcode >= 0x20) && (opcode <= 0x26)) || ((opcode >= 0x28) && (opcode <= 0x2B)) || (opcode == 0x2E) || (opcode == 0x30) || opcode == 0x31) || (opcode == 0x38) || (opcode == 0x3D))
+    if(((Shadow_IDEX.opcode >= 0x5) && (Shadow_IDEX.opcode <= 0xF)) || (Shadow_IDEX.opcode == 4) || ((Shadow_IDEX.opcode >= 0x20) && (Shadow_IDEX.opcode <= 0x26)) || ((Shadow_IDEX.opcode >= 0x28) && (Shadow_IDEX.opcode <= 0x2B)) || (Shadow_IDEX.opcode == 0x2E) || (Shadow_IDEX.opcode == 0x30) || (Shadow_IDEX.opcode == 0x31) || (Shadow_IDEX.opcode == 0x38) || (Shadow_IDEX.opcode == 0x3D))
     {
-        return std::vector<int> format ({itype, opcode, 0)};
+        return std::vector<int> format ({itype, Shadow_IDEX.opcode, 0)};
     }
     if((opcode == 2) || (opcode == 3)) 
     {
-        return std::vector<int> format({jtype, opcode, 0});
+        return std::vector<int> format({jtype, Shadow_IDEX.opcode, 0});
     }
     if(opcode == special_opcode1)                                  
     {
-        int func = find_func(instr);
-        return std::vector<int> format({special_opcode1l, opcode, func)};
+        Shadow_IDEX.func = find_func(instr);
+        return std::vector<int> format({special_opcode1l, Shadow_IDEX.opcode, Shadow_IDEX.func)};
     }
     if(opcode == special_opcode2)
     {
-        return std::vector<int> format({special_opcode2l, opcode, (instr && br_coprocessor_mask)>> 16});
+        return std::vector<int> format({special_opcode2l, Shadow_IDEX.opcode, (Shadow_IFID.instr && br_coprocessor_mask)>> 16});
     }
     if(opcode == special_opcode3)
     {
-        return std::vector<int> format({special_opcode3l, opcode, (instr && spec_opcode3_mask) >> 16});
+        return std::vector<int> format({special_opcode3l, Shadow_IDEX.opcode, (Shadow_IFID.instr && spec_opcode3_mask) >> 16});
 
                                    
 }
