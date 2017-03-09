342f1fe5f125a825860dda01bb656f783786c604
Anlo7085 <anlo7085@colorado.edu>
2017-03-08 19:20:02 -0700

    Update addi.cpp

--- Exe/addi.cpp
+++ Exe/addi.cpp
@@ -1,5 +1,5 @@
 int addi()
 {
-    Shadow_IFEX.rtvalue = Shadow_IFEX.rsvalue + Shadow_IFEX.immed16
+    Shadow_IFEX.ALUResult = Shadow_IFEX.rsvalue + Shadow_IFEX.immed16
     // Should I return this?
 }
