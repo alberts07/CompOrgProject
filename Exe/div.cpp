8b1db3861dd8db090ee5d5a7f473024c8f840390
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 16:31:20 -0700

    Update div.cpp

--- Exe/div.cpp
+++ Exe/div.cpp
@@ -1,4 +1,4 @@
 void div()
 {
-    Shadow_IDEX.ALUResult = Shadow_IDEX.RsValue / Shadow_IDEX.RtValue;
+    Shadow_IDEX.ALUResult = ((Shadow_IDEX.RsValue % Shadow_IDEX.RtValue) << 16) | (Shadow_IDEX.RsValue / Shadow_IDEX.RtValue);
 }
