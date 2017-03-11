49a25430548d1d0582299e4f7ba138445afd1746
alberts07 <aual4352@colorado.edu>
2017-03-11 16:39:59 -0700

    Update and rename Exe/divu.cpp to Exe/Unused_Ops/divu.cpp

--- Exe/divu.cpp
+++
@@ -1,4 +0,0 @@
-void divu()
-{
-  Shadow_IDEX.ALUResult = (((unsigned int)Shadow_IDEX.RsValue % (unsigned int)Shadow_IDEX.RtValue) << 16) | ((unsigned int)Shadow_IDEX.RsValue / (unsigned int)Shadow_IDEX.RtValue);
-}
