8977b1da3b5e6189afcc6c184caf8325f916efd8
alberts07 <aual4352@colorado.edu>
2017-03-11 16:39:09 -0700

    Update addiu.cpp

--- Exe/addiu.cpp
+++ Exe/addiu.cpp
@@ -1,4 +1,4 @@
 void addiu()
 {
-  (unsigned int)Shadow_IDEX.ALUResult = (unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.immed16;
+    (unsigned int)Shadow_IDEX.ALUResult = (unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.immed16;
 }
