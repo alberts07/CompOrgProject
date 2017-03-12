0c016f48e52fbf040ef1632980b7da4ea98eec73
alberts07 <aual4352@colorado.edu>
2017-03-11 17:57:01 -0700

    Update lhu.cpp

--- Exe/lhu.cpp
+++ Exe/lhu.cpp
@@ -1,15 +1,15 @@
 void lhu()
 {
-    unsigned int byte = ((unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.immed16) >> 2
-        switch (byte % 2){
+    unsigned int halfword = ((unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.immed16) >> 2
+        switch (halfword % 2){
             case 0:
                 {
-                    Shadow_IDEX.ALUResult = 0xFFFF0000 & byte;
+                    Shadow_IDEX.ALUResult = 0xFFFF0000 & halfword;
                     break;
                 }
             case 1:
                 {
-                    Shadow_IDEX.ALUResult = 0x0000FFFF & byte;
+                    Shadow_IDEX.ALUResult = 0x0000FFFF & halfword;
                     break;
                 }
         }
