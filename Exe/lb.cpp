a55d398cd915aaa5c2337bff4be461910d76cf9b
alberts07 <aual4352@colorado.edu>
2017-03-11 19:54:36 -0700

    Update lb.cpp

--- Exe/lb.cpp
+++ Exe/lb.cpp
@@ -1,29 +1,7 @@
 void lb()
 {
-    int byte = ((Shadow_IDEX.RsValue + Shadow_IDEX.immed16) >> 2);
-    switch(tmp % 4)
-    {
-        case 0: 
-        {
-            Shadow_EXMEM.ALUResult = 0xFF000000 & byte;
-            break;
-        }
-        case 1: 
-        {
-            Shadow_EXMEM.ALUResult = 0x00FF0000 & byte;           
-            break;
-        }
-        case 2: 
-        {
-            Shadow_EXMEM.ALUResult = 0x0000FF00 & byte;
-            break;
-        }
-        case 3:
-        {
-            Shadow_EXMEM.ALUResult = 0x000000FF & byte;
-            break;
-        }
-    }          
+    Shadow_EXMEM.ALUResult = ((Shadow_IDEX.RsValue + Shadow_IDEX.immed16) >> 2);
+    Shadow_EXMEM.byte = Shadow_EXMEM.ALUResult % 4;
 }
     
     
