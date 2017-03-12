5da0ef3af558e71101a11d3f8f55fe0d04a32b13
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 20:00:12 -0700

    Update Instr_MEM.cpp

--- Instr_MEM.cpp
+++ Instr_MEM.cpp
@@ -6,11 +6,49 @@
 //
 //
 
+case 0: 
+{   
+    Shadow_EXMEM.ALUResult = 0xFF000000 & byte;
+    break;
+}
+case 1: 
+{
+    Shadow_EXMEM.ALUResult = 0x00FF0000 & byte;           
+    break;
+}
+case 2: 
+{
+    Shadow_EXMEM.ALUResult = 0x0000FF00 & byte;
+    break;
+}
+case 3:
+{
+    Shadow_EXMEM.ALUResult = 0x000000FF & byte;
+    break;
+}
+
+
 void Instr_Mem()
 {
-        if(Shadow_EXMEM.MemRead)
-        {
-                        
+    if(Shadow_EXMEM.MemRead)
+    {
+        if(Shadow_EXMEM.half == true) 
+        {   
+            switch(Shadow_EXMEM.mem)
+            {
+                case 0: 
+                {
+                    memory[Shadow_EXMEM.ALUResult] & 0xFFFF0000;
+                    break;
+                }
+                case 1:
+                {
+                    memory[Shadow_EXMEM.ALUResult] & 0x0000FFFF;
+                    break;
+                }
+            }
+        }
+               
         }
         else if (Shadow_EXMEM.MemWrite)
         {
