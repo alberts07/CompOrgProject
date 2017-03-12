615218a28f9cfc76c369c01aaf4e8f25c7a4c5e6
alberts07 <aual4352@colorado.edu>
2017-03-11 17:53:29 -0700

    Update lbu.cpp

--- Exe/lbu.cpp
+++ Exe/lbu.cpp
@@ -4,19 +4,23 @@ void lbu()
         switch (byte % 4){
             case 0:
                 {
-                    Shadow_IDEX.ALUResult = 0xFF000000 & byte    
+                    Shadow_IDEX.ALUResult = 0xFF000000 & byte;
+                    break;
                 }
             case 1:
                 {
-                    Shadow_IDEX.ALUResult = 0x00FF0000 & byte    
+                    Shadow_IDEX.ALUResult = 0x00FF0000 & byte;
+                    break;
                 }
             case 2:
                 {
-                    Shadow_IDEX.ALUResult = 0x0000FF00 & byte    
+                    Shadow_IDEX.ALUResult = 0x0000FF00 & byte;
+                    break;
                 }
             case 3:
                 {
-                    Shadow_IDEX.ALUResult = 0x000000FF & byte    
+                    Shadow_IDEX.ALUResult = 0x000000FF & byte;
+                    break;
                 }
         }
 }
