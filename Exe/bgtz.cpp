abb76b1dad58cd2aeee1d6df2b827c1c7904b186
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:11:40 -0700

    Update bgtz.cpp

--- Exe/bgtz.cpp
+++ Exe/bgtz.cpp
@@ -1,6 +1,6 @@
 void bgtz()
 {
     if(Shadow_IDEX.RsValue > 0){
-        Shadow_IDEX.ALUResult = Shadow_IDEX.pcplus1+Shadow_IDEX.immed16;
+        Shadow_EXMEM.ALUResult = Shadow_IDEX.pcplus1+Shadow_IDEX.immed16;
     }
 }
