c4ff2451c9d288ae2c323ad1b4ad2fa0ad675e55
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 19:21:46 -0700

    Update Instr_MEM.cpp

--- Instr_MEM.cpp
+++ Instr_MEM.cpp
@@ -5,3 +5,17 @@
 //  Created by Austin Alberts on 3/11/17.
 //
 //
+
+void Instr_Mem(int format)
+{
+    if(format == itype)
+    {
+        if(Shadow_EXMEM.MemRead || Shadow_EXMEM.MemWrite)
+        {
+        
+            
+        }
+    }
+}
+    
+    
