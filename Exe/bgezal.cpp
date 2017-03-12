c3789685d816be7fb1138c237c31106c1b5b6035
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:11:30 -0700

    Update bgezal.cpp

--- Exe/bgezal.cpp
+++ Exe/bgezal.cpp
@@ -3,6 +3,6 @@ void bgezal()
     if(Shadow_IDEX.RsValue >= 0)
     {
         Reg[31] = $pc + 2;
-        Shadow_IDEX.ALUResult = Shadow_IDEX.pcplus1 + Shadow_IDEX.immed16;
+        Shadow_EXMEM.ALUResult = Shadow_IDEX.pcplus1 + Shadow_IDEX.immed16;
     }
 }        
