8341c4df0e9af2e0bd69b8281141c1ae5df4fe00
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:10:08 -0700

    Update addu.cpp

--- Exe/addu.cpp
+++ Exe/addu.cpp
@@ -1,4 +1,4 @@
 void addu()
 {
-    (unsigned int)Shadow_IDEX.ALUResult = (unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.RtValue;
+    (unsigned int)Shadow_EXMEM.ALUResult = (unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.RtValue;
 }
