b2e888b3a97bd254ce4cc1de19a48a0f59c086db
alberts07 <aual4352@colorado.edu>
2017-03-11 19:45:56 -0700

    Update sh.cpp

--- Exe/sh.cpp
+++ Exe/sh.cpp
@@ -1,17 +1,5 @@
 void sh()
 {
-    int halfword = (Shadow_IDEX.RsValue + Shadow_IDEX.immed16) >> 2;
-    switch(halfword % 2)
-    {
-        case 0: 
-        {
-            Shadow_EXMEM.ALUResult = halfword & 0xFFFF0000;
-            break;
-        }
-        case 1: 
-        {
-            Shadow_EXMEM.ALUResult = halfword & 0x0000FFFF;            
-            break;
-        }
-    }
+    Shadow_EXMEM.ALUResult = (Shadow_IDEX.RsValue + Shadow_IDEX.immed16) >> 2;
+    Shadow_EXMEM.half = Shadow_EXMEM.ALUResult % 2    
 }
