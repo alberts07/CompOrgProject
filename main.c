1c9cc1374f57a39e456e011332d904109eb297f4
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 21:13:02 -0700

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
-int holder[4] = {0x8fc20018, 0x00021080, 0x27c30018, 0x00621021};
+int holder[4] = {0x00004020, 0x2009000F, 0x0800000C, 0x00095080};
+//0x00004020 = add $t0, 0, 0
+//0x2009000F = addi t1, zero, 15
+//0x08000004 = j 0x00C
+//0x00095080 = sll t2, t1, 2 
+
 int memory[0x50000000];
 int $pc = 0x00000000;
 int Reg[32] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
@@ -6,13 +11,19 @@ int Reg[32] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
 
 int main()
 {
-    for(;;)
+    for(int i = 0; i < 4; i++)
     {
-        Instr_IF();
+        memory[$pc] = holder[i];
+        $pc++;
+    }
+    $pc = 0;
+    for($pc = 0; $pc < 4; $pc++)
+    {
+        Instr_IF(memory[$pc]);
         Instr_WB();
         Instr_ID();
         Instr_Exe();
         Instr_Mem();
-        Update_State()
+        Update_State();
     }
 }
