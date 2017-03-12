bd17f5b82a210b5e2746a8aee62ed4a2e5abee5e
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:21:02 -0700

    Update subu.cpp

--- Exe/subu.cpp
+++ Exe/subu.cpp
@@ -1,4 +1,4 @@
 void subu()
 {
-    (unsigned int)Shadow_IDEX.ALUResult = (unsigned int)Shadow_IDEX.RsValue - (unsigned int)Shadow_IDEX.RtValue;
+    (unsigned int)Shadow_EXMEM.ALUResult = (unsigned int)Shadow_IDEX.RsValue - (unsigned int)Shadow_IDEX.RtValue;
 }
