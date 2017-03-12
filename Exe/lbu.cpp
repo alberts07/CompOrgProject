fa98f4c4beb03f0f933c8995a5e2fde25f2ccaaa
alberts07 <aual4352@colorado.edu>
2017-03-11 19:56:38 -0700

    Update lbu.cpp

--- Exe/lbu.cpp
+++ Exe/lbu.cpp
@@ -1,26 +1,5 @@
 void lbu()
 {
-    unsigned int byte = ((unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.immed16) >> 2
-        switch (byte % 4){
-            case 0:
-                {
-                    Shadow_EXMEM.ALUResult = 0xFF000000 & byte;
-                    break;
-                }
-            case 1:
-                {
-                    Shadow_EXMEM.ALUResult = 0x00FF0000 & byte;
-                    break;
-                }
-            case 2:
-                {
-                    Shadow_EXMEM.ALUResult = 0x0000FF00 & byte;
-                    break;
-                }
-            case 3:
-                {
-                    Shadow_EXMEM.ALUResult = 0x000000FF & byte;
-                    break;
-                }
-        }
+     Shadow_EXMEM.ALUResult = ((unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.immed16) >> 2
+     Shadow_EXMEM.byte = Shadow_EXMEM.ALUResult % 4
 }
