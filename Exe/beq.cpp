61b20650fb7d07991a94aa743f813b25e3f4cc67
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:11:03 -0700

    Update beq.cpp

--- Exe/beq.cpp
+++ Exe/beq.cpp
@@ -1,5 +1,5 @@
 void beq()
 {
     if(Shadow_IDEX.RsValue == Shadow_IDEX.RtValue)
-        Shadow_IDEX.ALUResult = Shadow_IDEX.pcplus1 + Shadow_IDEX.immed16;
+        Shadow_EXMEM.ALUResult = Shadow_IDEX.pcplus1 + Shadow_IDEX.immed16;
 }
