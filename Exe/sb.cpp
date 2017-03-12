576c3375bd6b1feb736f97efdc47d465b29f673a
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:18:06 -0700

    Update sb.cpp

--- Exe/sb.cpp
+++ Exe/sb.cpp
@@ -5,22 +5,22 @@ void sb()
     {
         case 0: 
         {   
-            Shadow_IDEX.ALUResult = 0xFF000000 & byte;
+            Shadow_EXMEM.ALUResult = 0xFF000000 & byte;
             break;
         }
         case 1: 
         {
-            Shadow_IDEX.ALUResult = 0x00FF0000 & byte;           
+            Shadow_EXMEM.ALUResult = 0x00FF0000 & byte;           
             break;
         }
         case 2: 
         {
-            Shadow_IDEX.ALUResult = 0x0000FF00 & byte;
+            Shadow_EXMEM.ALUResult = 0x0000FF00 & byte;
             break;
         }
         case 3:
         {
-            Shadow_IDEX.ALUResult = 0x000000FF & byte;
+            Shadow_EXMEM.ALUResult = 0x000000FF & byte;
             break;
         }
     }          
