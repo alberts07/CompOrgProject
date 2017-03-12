6e98cd9f71b25b40ec2cc0df4891f0d4441dcfb0
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 19:38:13 -0700

    Update Instr_WB.cpp

--- Instr_WB.cpp
+++ Instr_WB.cpp
@@ -8,3 +8,14 @@
 
 void Instr_WB(int format)
 {
+    if((Shadow_MEMWB.RegWrite == true) && (format == itype))
+    {
+        Reg[Shadow_IDEX.Rt] = Shadow_IDEX.ALUResult;
+    }
+    if((Shadow_MEMWB.RegWrite == true) && (format == rtype))
+    {
+        Reg[Shadow_IDEX.Rd] = Shadow_IDEX.ALUResult;
+    }
+}
+           
+ 
