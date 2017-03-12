c80eea9cdf452a4b5bdbc18036dcc7ff8948fb15
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:12:07 -0700

    Update bltz.cpp

--- Exe/bltz.cpp
+++ Exe/bltz.cpp
@@ -2,6 +2,6 @@ void bltz()
 {
     if(Shadow_IDEX.RsValue < 0)
     {
-        Shadow_IDEX.ALUResult = Shadow_IDEX.pcplus1 + Shadow_IDEX.immed16;
+        Shadow_EXMEM.ALUResult = Shadow_IDEX.pcplus1 + Shadow_IDEX.immed16;
     }
 }
