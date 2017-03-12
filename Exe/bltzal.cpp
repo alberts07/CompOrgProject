ee1416ef2dc67d140b71a52f6485d159ccb28d08
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:14:32 -0700

    Update bltzal.cpp

--- Exe/bltzal.cpp
+++ Exe/bltzal.cpp
@@ -3,6 +3,6 @@ void bltzal()
     if (Shadow_IDEX.RsValue < 0)
     {
         Reg[31]= $pc + 2;
-        Shadow_EXMEM.ALUResult = Shadow_IDEX.RsValue + Shadow_IDEX.immed16;
+        Shadow_EXMEM.ALUResult = Shadow_IDEX.pcplus1 + Shadow_IDEX.immed16;
     }
 }
