eae619bb2803a6bacbc3a48b2648fd2adba6d320
alberts07 <aual4352@colorado.edu>
2017-03-11 20:05:57 -0700

    Update lbu.cpp

--- Exe/lbu.cpp
+++ Exe/lbu.cpp
@@ -1,6 +1,6 @@
 void lbu()
 {
      Shadow_EXMEM.ALUResult = ((unsigned int)Shadow_IDEX.RsValue + (unsigned int)Shadow_IDEX.immed16) >> 2;
-     Shadow_EXMEM.byte = Shadow_EXMEM.ALUResult % 4;
+     Shadow_EXMEM.mem = Shadow_EXMEM.ALUResult % 4;
      Shadow_EXMEM.half = false;
 }
