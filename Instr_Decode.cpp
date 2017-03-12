1c3864d3cf155b0ab39313bd3bc5a20947efab91
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 18:55:43 -0700

    Update Instr_Decode.cpp

--- Instr_Decode.cpp
+++ Instr_Decode.cpp
@@ -8,27 +8,52 @@
 #include "Instr_Decode.hpp"
 #include "IF.hpp"
 
-std::vector<int> Instr_Decode(unsigned int instr)
+int Instr_Decode()
 {
-    std::vector<int> format = find_format(instr);
+    Shadow_IDEX.instr = Shadow_IFID.instr;
+    Shadow_IDEX.pcplus1 = Shadow_IFID.pcplus1;
+    
+    std::vector<int> format = find_format(Shadow_IDEX.instr);
     if(format[0] == rtype)
     {
-        Shadow_IDEX.Rs = find_rs(Shadow_IFID.instr);
-        Shadow_IDEX.Rt = find_rt(Shadow_IFID.instr);
-        Shadow_IDEX.Rd = find_rd(Shadow_IFID.instr);
-        Shadow_IDEX.shamt = find_shamt(Shadow_IFID.instr);
+        
+        Shadow_IDEX.Rs = find_rs(Shadow_IDEX.instr);
+        Shadow_IDEX.RsValue = Reg[Shadow_IDEX.Rs];
+        Shadow_IDEX.Rt = find_rt(Shadow_IDEX.instr);
+        Shadow_IDEX.RtValue = Reg[Shadow_IDEX.Rt];      
+        Shadow_IDEX.Rd = find_rd(Shadow_IDEX.instr);
+        Shadow_IDEX.RdValue = Reg[Shadow_IDEX.Rd]; 
+        Shadow_IDEX.shamt = find_shamt(Shadow_IDEX.instr);
+        Shadow_IDEX.RegWrite = true;
+        Shadow_IDEX.MemWrite = false;
+        Shadow_IDEX.MemRead = false;
+        Shadow_IDEX.MemtoReg = false;
+        Shadow_IDEX.ALUSrc = true;
+        Shadow_IDEX.opcode = format[1];
+        Shadow_IDEX.func = format[2];
+        return rtype;
     } 
          
     if(format[0] == itype)
-    {
-        Shadow_IDEX.immed16 = find_immed16(Shadow_IFID.instr);
-        Shaodw_IDEX.Rs = find_rs(Shadow_IFID.instr);
-        Shadow_IDEX.Rt = find_rt(Shadow_IFID.instr);
+    { 
+        Shadow_IDEX.ALUSrc = true;
+        Shadow_IDEX.immed16 = Shadow_IFID.immed16;
+        Shaodw_IDEX.Rs = find_rs(Shadow_IDEX.instr);
+        Shadow_IDEX.RsValue = Reg[Shadow_IDEX.Rs];
+        Shadow_IDEX.Rt = find_rt(Shadow_IDEX.instr);
+        Shadow_IDEX.RtValue = Reg[Shadow_IDEX.Rt];
+        Shadow_IDEX.opcode = format[1];
+        return itype;
     }  
     if(format[0] == jtype)
     {
-        Shadow_IDEX.immed26 = find_immed26(Shadow_IFID.instr);
-           
+        Shadow_IDEX.RegWrite = false;
+        Shadow_IDEX.MemWrite = false;
+        Shadow_IDEX.MemRead = false;
+        Shadow_IDEX.MemtoReg = false;
+        Shadow_IDEX.ALUSrc = false;
+        Shadow_IDEX.immed26 = find_immed26(Shadow_IDEX.instr);
+        return jtype;
     }
     /*
     if(format[0] == special_opcode1l)
