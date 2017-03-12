e092d388239dc61db0fde1b43f3a548ab1e1f399
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 20:06:34 -0700

    Update Instr_MEM.cpp

--- Instr_MEM.cpp
+++ Instr_MEM.cpp
@@ -48,6 +48,38 @@ void Instr_Mem()
                 }
             }
         }
+        if(Shadow_EXMEM.half == false)
+        {
+            switch(Shadow_EXMEM.mem)
+            {
+                case 0: 
+                {
+                    memory[Shadow_EXMEM.ALUResult] & 0xFF000000;
+                    break;
+                }
+                case 1:
+                {
+                    memory[Shadow_EXMEM.ALUResult] & 0x00FF0000;
+                    break;
+                }
+                case 2: 
+                {
+                    memory[Shadow_EXMEM.ALUResult] & 0x0000FF00;
+                    break;
+                }
+                case 3:
+                {
+                    memory[Shadow_EXMEM.ALUResult] & 0x000000FF;
+                    break;
+                }
+                case 4: 
+                {
+                    memory[Shadow_EXMEM.ALUResult] & 0xFFFFFFFF;
+                    break;
+                }
+            } 
+        }
+    }
                
         }
         else if (Shadow_EXMEM.MemWrite)
