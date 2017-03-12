78b33b63f2ea9462f23cd8be9b9b240b69bb0da3
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 19:03:46 -0700

    Update Instr_IF.cpp

--- Instr_IF.cpp
+++ Instr_IF.cpp
@@ -1,1 +1,10 @@
+extern int $pc;
+extern int memory[10000];
+#include "Instr_IF.h"
 
+void Instr_IF(unsigned int instruction)
+{
+    Shadow_IFID.instr = instruction;
+    Shadow_IFID.pcplus1 = $pc + 1; 
+}
+  
