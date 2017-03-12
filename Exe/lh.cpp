ece7f08667287ddeecf7143bd412605b1954f22b
Anlo7085 <anlo7085@colorado.edu>
2017-03-11 17:54:33 -0700

    Create lh.cpp

---
+++ Exe/lh.cpp
@@ -0,0 +1,17 @@
+void lh()
+{
+    int halfword = (Shadow_IDEX.RsValue + Shadow_IDEX.immed16) >> 2;
+    switch(halfword % 2)
+    {
+        case 0: 
+        {
+            Shadow_IDEX.ALUResult = halfword & 0xFFFF0000;
+            break;
+        }
+        case 1: 
+        {
+            Shadow_IDEX.ALUResult = halfword & 0x0000FFFF;            
+            break;
+        }
+    }
+}
