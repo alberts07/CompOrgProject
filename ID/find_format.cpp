19ec09b813730d921960701ca0bbdbd88f2e7619
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-03-13 15:12:36 -0600

    updates

--- ID/find_format.cpp
+++ ID/find_format.cpp
@@ -1,23 +1,25 @@
 #include <vector>
 #include <iostream>
+
+
 std::vector<int> find_format(unsigned int instr)
 {
     Shadow_IDEX.opcode = find_opcode(instr);
     if(Shadow_IDEX.opcode == 0)
     {
         Shadow_IDEX.func = find_func(instr)
-        return std::vector<int> format ({rtype, Shadow_IDEX.opcode, Shadow_IDEX.func}); 
+        return std::vector<int> format ({rtype, Shadow_IDEX.opcode, Shadow_IDEX.func});
     }
     if(((Shadow_IDEX.opcode >= 0x5) && (Shadow_IDEX.opcode <= 0xF)) || (Shadow_IDEX.opcode == 4) || ((Shadow_IDEX.opcode >= 0x20) && (Shadow_IDEX.opcode <= 0x26)) || ((Shadow_IDEX.opcode >= 0x28) && (Shadow_IDEX.opcode <= 0x2B)) || (Shadow_IDEX.opcode == 0x2E) || (Shadow_IDEX.opcode == 0x30) || (Shadow_IDEX.opcode == 0x31) || (Shadow_IDEX.opcode == 0x38) || (Shadow_IDEX.opcode == 0x3D))
     {
         return std::vector<int> format ({itype, Shadow_IDEX.opcode, 0)};
     }
-    if((opcode == 2) || (opcode == 3)) 
+    if((opcode == 2) || (opcode == 3))
     {
         return std::vector<int> format({jtype, Shadow_IDEX.opcode, 0});
     }
                                         /*
-    if(opcode == special_opcode1)                                  
+    if(opcode == special_opcode1)
     {
         Shadow_IDEX.func = find_func(instr);
         return std::vector<int> format({special_opcode1l, Shadow_IDEX.opcode, Shadow_IDEX.func)};
@@ -31,5 +33,5 @@ std::vector<int> find_format(unsigned int instr)
         return std::vector<int> format({special_opcode3l, Shadow_IDEX.opcode, (Shadow_IFID.instr && spec_opcode3_mask) >> 16});
     }
 */
-                                   
+
 }
