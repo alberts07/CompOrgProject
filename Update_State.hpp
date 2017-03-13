7433c75afc3e9840e75bb4faa867b5fb066f0fe5
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-03-13 14:46:24 -0600

    updated function

--- Update_State.hpp
+++ Update_State.hpp
@@ -1,7 +1,15 @@
 //
 //  Update_State.hpp
-//  
+//
 //
 //  Created by Austin Alberts on 3/11/17.
 //
 //
+
+#ifndef Update_State_hpp
+#define Update_State_hpp
+
+
+void Update_State(void);
+
+#endif
