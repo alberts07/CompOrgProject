01995d65679deda1229ce980c3899848607a493d
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-03-13 14:47:45 -0600

    updated id.hpp

--- Instr_ID.hpp
+++ Instr_ID.hpp
@@ -1,17 +1,11 @@
-//
-//  Single_Cycle.hpp
-//  
-//
-//  Created by Drew Lockwood on 3/1/17.
-//
-//
 
-#ifndef Single_Cycle_hpp
-#define Single_Cycle_hpp
+#ifndef Instr_ID_hpp
+#define Instr_ID_hpp
 
 #include <vector>
 #include <iostream>
 
+
 int Instr_ID(void);
 
-#endif /* Single_Cycle_hpp */
+#endif
