ddddb931ec132af307d00eb02fbaca81f245c329
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-03-13 14:54:47 -0600

    updates

--- Instr_WB.cpp
+++ Instr_WB.cpp
@@ -1,19 +1,17 @@
-//
-//  Instr_WB.cpp
-//
-//
-//  Created by Austin Alberts on 3/11/17.
-//
-//
+#include "Instr_IF_hpp"
+#include "Instr_ID_hpp"
+
+extern struct memwb Shadow_MEMWB;
+extern struct idex Shadow_IDEX;
 
 void Instr_WB(int format)
 {
     if((Shadow_MEMWB.RegWrite == true) && (format == itype))
     {
-        Reg[Shadow_IDEX.Rt] = Shadow_IDEX.ALUResult;
+        Reg[Shadow_IDEX.Rt] = Shadow_EXMEM.ALUResult;
     }
     if((Shadow_MEMWB.RegWrite == true) && (format == rtype))
     {
-        Reg[Shadow_IDEX.Rd] = Shadow_IDEX.ALUResult;
+        Reg[Shadow_IDEX.Rd] = Shadow_EXMEM.ALUResult;
     }
 }
