20435e069779503697193d70abc07e647ebb2326
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-03-18 17:27:06 -0600

    Fixing vector formatting

--- decode.cpp
+++ decode.cpp
@@ -11,15 +11,15 @@ std::vector<int> find_format(unsigned int instr)
     if(Shadow_IDEX.opcode == 0)
     {
         Shadow_IDEX.func = find_func(instr);
-        return std::vector<int> format ({rtype, Shadow_IDEX.opcode, Shadow_IDEX.func});
+        return std::vector<int> v[3] = {rtype, Shadow_IDEX.opcode, Shadow_IDEX.func};
     }
     if(((Shadow_IDEX.opcode >= 0x5) && (Shadow_IDEX.opcode <= 0xF)) || (Shadow_IDEX.opcode == 4) || ((Shadow_IDEX.opcode >= 0x20) && (Shadow_IDEX.opcode <= 0x26)) || ((Shadow_IDEX.opcode >= 0x28) && (Shadow_IDEX.opcode <= 0x2B)) || (Shadow_IDEX.opcode == 0x2E) || (Shadow_IDEX.opcode == 0x30) || (Shadow_IDEX.opcode == 0x31) || (Shadow_IDEX.opcode == 0x38) || (Shadow_IDEX.opcode == 0x3D))
     {
-        return std::vector<int> format ({itype, Shadow_IDEX.opcode, 0)};
+        return std::vector<int> v[3] = {itype, Shadow_IDEX.opcode, 0};
     }
     if((opcode == 2) || (opcode == 3))
     {
-        return std::vector<int> format({jtype, Shadow_IDEX.opcode, 0});
+        return std::vector<int> v[3] = {jtype, Shadow_IDEX.opcode, 0};
     }
                                         /*
     if(opcode == special_opcode1)
