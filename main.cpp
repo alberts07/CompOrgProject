74d57691ffdd1bfca759e85c489bf4c87a675d6d
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-03-31 07:19:37 -0700

    Commented main
    Updated main to make it easier to understand.

--- main.cpp
+++ main.cpp
@@ -8,39 +8,44 @@
 #include <string>
 
 int nop_instr = 0x00000000;
+// This holder array contains the machine code functions that we test.
+//To add a function, put it at the bottom of this, with the function
+//next to it
 unsigned int holder[] = {
-  0x00004020,  //0x00004020 = add $t0, $zero, $zero
-  0x21290FFF,  //0x20090FFF = addi $t1, $t1, 0x0FFF
-  0x00095080,  //0x00095080 = sll $t2, $t1, 2
-  0x01495822,  //0x01495822 = sub $t3, $t2, $t1
-  0x3529F000,  //0x3529001f = ori $t1, $t1, 0xF000
-  0x21080002,  //0x21080002 = addi $t0, $t0, 2
-  0x010B682A,  //0x010b682A = slt $t5, $t0, $t3
-  0x31290010,  //0x31290010 = andi $t1, $t1, 0x10
-  0x00094902,  //0x00094902 = srl $t1, $t1, 4
-  0x00004020,  //0x00004020 = add $t0, $zero, $zero
-  0x01284826,  //0x01284826 = xor $t1, $t1, $t0
-  0x39290004,  //0x35290004 = xori $t1, $t1, 0x4
-  0x29290006,  //0x29290006 = slti  $t1, $t1, 0x6
-  0x2d29FFF0,  //0x2d29FFFF = sltiu $t1, $t1, 0xFFF0
-  0x3508F00F,  //0x3508F00F = ori $t0, $t0, 0xF00F
-  0x3529F00F,  //0x3529F00F = ori $t1, $t1, 0xF00F
-  0x01284823,  //0x01284823 = subu $t1, $t1, $t0
-  0x00004020,  //0x00004020 = add $t0, $zero, $zero
-  0x2108FFFF,  //0x20090FFF = addi $t0, $t0, 0xFFFF
-  0x01094827,  //0x01094825 = nor $t1, $t0, $t1
-  0x2108FFFF,  //0x20090FFF = addi $t0, $t0, 0xFFFF
-  0x00005820,  //0x00005820 = add $t3, $zero, $zero
-  0x216B0004,  //0x216B0001 = addi, $t3, $t3, 0x4
-  0xAD6B0000,  //0xAD6B0000 = sw $t3, 0($t3)
-  0x8D680000,  //0x8D680000 = lw $t0, 0($t3)
-  0x00004020,  //0x00004020 = add $t0, $zero, $zero
-  0x016B5820,  //0x016B5820 = add $t3, $t3, $t3
-  0xA16B0022,  //0xA16B0000 = sb $t3, 22($t3)
-  0x81690022  //0x81690002 = lb $t1, 22($t3)
+  0x00004020,  //add $t0, $zero, $zero
+  0x21290FFF,  //addi $t1, $t1, 0x0FFF
+  0x00095080,  //sll $t2, $t1, 2
+  0x01495822,  //sub $t3, $t2, $t1
+  0x3529F000,  //ori $t1, $t1, 0xF000
+  0x21080002,  //addi $t0, $t0, 2
+  0x010B682A,  //slt $t5, $t0, $t3
+  0x31290010,  //andi $t1, $t1, 0x10
+  0x00094902,  //srl $t1, $t1, 4
+  0x00004020,  //add $t0, $zero, $zero
+  0x01284826,  //xor $t1, $t1, $t0
+  0x39290004,  //xori $t1, $t1, 0x4
+  0x29290006,  //slti  $t1, $t1, 0x6
+  0x2d29FFF0,  //sltiu $t1, $t1, 0xFFF0
+  0x3508F00F,  //ori $t0, $t0, 0xF00F
+  0x3529F00F,  //ori $t1, $t1, 0xF00F
+  0x01284823,  //subu $t1, $t1, $t0
+  0x00004020,  //add $t0, $zero, $zero
+  0x2108FFFF,  //addi $t0, $t0, 0xFFFF
+  0x01094827,  //nor $t1, $t0, $t1
+  0x2108FFFF,  //addi $t0, $t0, 0xFFFF
+  0x00005820,  //add $t3, $zero, $zero
+  0x216B0004,  //addi, $t3, $t3, 0x4
+  0xAD6B0000,  //sw $t3, 0($t3)
+  0x8D680000,  //lw $t0, 0($t3)
+  0x00004020,  //add $t0, $zero, $zero
+  0x016B5820,  //add $t3, $t3, $t3
+  0xA16B0022,  //sb $t3, 22($t3)
+  0x81690022  //lb $t1, 22($t3)
 };
 
-
+//This array contains the functions so that the user can visially
+//debug each function. Once you add machine code to the top,
+//add the function to the bottom of this array.
 std::string names[(sizeof(holder)/sizeof(*holder))] = {
  "add $t0, $zero, $zero",
  "addi $t1, $t1, 0x0FFF",
@@ -98,12 +103,14 @@ int main()
     unsigned int clock_cycles = 0;
     int format = 10;
     int i = 0;
+    //Transfer holder array to memory
     for(i = 0; i < (sizeof(holder)/sizeof(*holder)); i++)
     {
         memory[$pc] = holder[i];
         $pc++;
     }
     $pc = 0;
+    //Run pipeline
     for(i  = 0; i < (sizeof(holder)/sizeof(*holder)); i++)
     {
         Instr_IF(memory[$pc]);
@@ -113,7 +120,7 @@ int main()
         Instr_WB(format);
         Update_State();
         clock_cycles++;
-        if(i != 23 && i > 10 && i != 27)
+        if(i != 23  && i != 27) //Print out all except stores
             std::cout << names[$pc] << ":  " << Reg[MEMWB.DstReg] << std::endl;
         else if (i == 23 || i == 27)
             std::cout << names[$pc] << std::endl;
