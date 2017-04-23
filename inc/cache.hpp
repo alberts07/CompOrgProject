3664fe36c56577ac2d8c893b7a3efed99d458691
user <aual4352@colorado.edu>
2017-04-22 22:58:43 -0600

    Compiling now

--- inc/cache.hpp
+++ inc/cache.hpp
@@ -7,31 +7,32 @@
 //
 #ifndef cache_h
 #define cache_h
-
+bool WRITE_BACK = 0;
 class cache{
-  bool valid[];
-  bool dirty[];
-  int data[];
-  unsigned int tag[];
-  bool DONE;
-  int cache_hit;
-  int cache_access;
-  int cache_size;
-  int block_size;
+  public:
+    bool valid[];
+    bool dirty[];
+    int data[];
+    unsigned int tag[];
+    bool DONE;
+    int cache_hit;
+    int cache_access;
+    int cache_size;
+    int block_size;
 
-  unsigned int addrtag;
-  int block_address;
-  int block_offset;
-  int byte_offset;
+    unsigned int addrtag;
+    int block_address;
+    int block_offset;
+    int byte_offset;
 
-  cache (int size, int block);
-  unsigned int get_tag(unsigned int addr);
-  unsigned int get_block(unsigned int addr);
-  unsigned int get_block_offset(unsigned int addr);
-  bool is_valid(unsigned int addr);
-  bool is_dirty(unsigned int addr);
-  void read_cache(cache, unsigned int addr, unsigned int *data);
-  void write_cache(unsigned int addr, unsigned int *blockmemdata);
+    cache (int size, int block);
+    unsigned int get_tag(unsigned int addr);
+    unsigned int get_block(unsigned int addr);
+    unsigned int get_block_offset(unsigned int addr);
+    bool is_valid(unsigned int addr);
+    bool is_dirty(unsigned int addr);
+    void read_cache(cache, unsigned int addr, unsigned int *data);
+    void write_cache(unsigned int addr, unsigned int *blockmemdata);
 };
 
 #endif
