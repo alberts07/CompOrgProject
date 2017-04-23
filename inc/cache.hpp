c137df962d37cf4e6c75ecd317ff5d385a28fd24
user <aual4352@colorado.edu>
2017-04-22 22:41:35 -0600

    attempt at making

--- inc/cache.hpp
+++ inc/cache.hpp
@@ -1,17 +1,37 @@
 //
-//  icache.hpp
+//  cache.hpp
 //
 //
 //  Created by Austin Alberts on 4/16/17.
 //
 //
+#ifndef cache_h
+#define cache_h
 
-#ifndef icache_h
-#define icache_h
+class cache{
+  bool valid[];
+  bool dirty[];
+  int data[];
+  unsigned int tag[];
+  bool DONE;
+  int cache_hit;
+  int cache_access;
+  int cache_size;
+  int block_size;
 
+  unsigned int addrtag;
+  int block_address;
+  int block_offset;
+  int byte_offset;
 
-void icache(unsigned int instruction, int cache_size, int block_size);
-
-void dcache(unsigned int memory, int cache_size, int block_size);
+  cache (int size, int block);
+  unsigned int get_tag(unsigned int addr);
+  unsigned int get_block(unsigned int addr);
+  unsigned int get_block_offset(unsigned int addr);
+  bool is_valid(unsigned int addr);
+  bool is_dirty(unsigned int addr);
+  void read_cache(cache, unsigned int addr, unsigned int *data);
+  void write_cache(unsigned int addr, unsigned int *blockmemdata);
+};
 
 #endif
