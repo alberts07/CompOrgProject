fb452b7435f3b97f17f6d04e7a2436ac01b8e81c
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-04-28 13:10:41 -0600

    Updates

---
+++ inc/Program1.hpp
@@ -0,0 +1,8 @@
+
+#ifndef program1_h
+#define program1_h
+
+
+void transfer_Program1(void);
+
+#endif
---
+++ inc/Program2.hpp
@@ -0,0 +1,8 @@
+
+#ifndef program2_h
+#define program2_h
+
+
+void transfer_Program2(void);
+
+#endif
