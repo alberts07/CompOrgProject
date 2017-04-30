652bf96f9b14d2d0ed28d77c0d557bcd2c55c772
Andrew Lockwood <andrew.lockwood@colorado.edu>
2017-04-30 16:08:18 -0600

    updates

--- src/Update_State.cpp
+++ src/Update_State.cpp
@@ -2,13 +2,17 @@
 #include "Instr_IF.hpp"
 #include "Update_State.hpp"
 
-extern struct ifid Shadow_IFID, IFID;
-extern struct idex Shadow_IDEX, IDEX;
-extern struct exmem Shadow_EXMEM, EXMEM;
-extern struct memwb Shadow_MEMWB, MEMWB;
+extern struct ifid Shadow_IFID, IFID, IFID_1;
+extern struct idex Shadow_IDEX, IDEX, IDEX_1;
+extern struct exmem Shadow_EXMEM, EXMEM, EXMEM_1;
+extern struct memwb Shadow_MEMWB, MEMWB, MEMWB_1;
 
 void Update_State()
 {
+    IFID_1 = IFID;
+    IDEX_1 = IDEX;
+    EXMEM_1 = EXMEM;
+    MEMWB_1 = MEMWB;
     IFID = Shadow_IFID;
     IDEX = Shadow_IDEX;
     EXMEM = Shadow_EXMEM;
